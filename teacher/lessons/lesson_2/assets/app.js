95b08d81aa18a6915a235972ba4ef5c4181fb491
Farkhod <33034567+Farkhod007@users.noreply.github.com>
2021-09-04 08:47:12 +0500

    Add lesson 2

---
+++ teacher/lessons/lesson_2/assets/app.js
@@ -0,0 +1,12 @@
+function myFunction() {
+	document.getElementById('para1').innerHTML = 'Paragraph Changed'
+}
+
+// window.alert("Do you agee?");
+
+// console.log("Alert accepted!");
+
+let x, y, z
+x = 5
+y = 6
+z = x + y

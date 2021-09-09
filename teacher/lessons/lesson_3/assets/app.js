8fb1e3ced4df1781b57f5f2d4b96d116ab65c4ca
Farkhod <33034567+Farkhod007@users.noreply.github.com>
2021-09-09 09:47:07 +0500

    Add lesson 3

---
+++ teacher/lessons/lesson_3/assets/app.js
@@ -0,0 +1,41 @@
+const input = document.getElementById('search')
+const elements = document.getElementById('elements')
+
+let items = [
+	{
+		icon: '../assets/images/home.svg',
+		title: 'Property',
+	},
+	{
+		icon: '../assets/images/car.svg',
+		title: 'Car',
+	},
+	{
+		icon: '../assets/images/boat.svg',
+		title: 'Boat',
+	},
+	{
+		icon: '../assets/images/Carpet.svg',
+		title: 'Carpet',
+	},
+]
+
+let filteredItems = items
+
+function filterItems() {
+	filteredItems = items.filter((item) =>
+		item.title.toLowerCase().includes(input.value.toLowerCase())
+	)
+
+	display()
+}
+
+function display() {
+	let list = "<ul>"
+	filteredItems.map((item) => list += "<li>" + item.title + "</li>")
+	list += "</ul>"
+
+	elements.innerHTML = list
+}
+
+display()
